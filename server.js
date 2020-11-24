320d66bcda7622384d269be0ba4b01bc0b6b03da
Don Vidath M Weerasinghe <vidath@msn.com>
2020-11-24 10:26:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.get('/', function(req, res){
+ res.json({"tutorial" : "Build REST API with node.js"});
+});
+app.listen(3000, function(){ console.log('Node server listening on port 3000');});
